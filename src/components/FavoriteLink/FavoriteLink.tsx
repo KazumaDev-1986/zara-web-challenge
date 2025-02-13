f7f173dfb9d0b544779e3725ae789a8a20dc08ae
KazumaDev-1986 <kazuma.dev1986@outlook.com>
2025-02-13 16:28:02 +0100

    feat: Add SVG assets and update components
    - Added SVG files to public/assets folder
    - Updated Card, ToggleFavoriteButton, FavoriteLink, and SearchInput components
    - Refactored SearchInput test

--- src/components/FavoriteLink/FavoriteLink.tsx
+++ src/components/FavoriteLink/FavoriteLink.tsx
@@ -23,21 +23,7 @@ export const FavoriteLink: FC = () => {
   return (
     <button onClick={onClick}>
       <div className="favorite-link">
-        <svg
-          className="icon"
-          width="24"
-          height="22"
-          viewBox="0 0 24 22"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            clipRule="evenodd"
-            d="M12 3.63869L6 -0.00292969L0 3.63869V11.4422L12 21.6734L24 11.4422V3.63869L18 -0.00292969L12 3.63869Z"
-            fill="#EC1D24"
-          />
-        </svg>
+        <img className="icon" src="/assets/heart-active.svg" />
         <span className="counter">{ctx?.state.favorites.length ?? 0}</span>
       </div>
     </button>
--- src/components/SearchInput/SearchInput.test.tsx
+++ src/components/SearchInput/SearchInput.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { SearchInput } from './SearchInput';
+import { I18nextProvider } from 'react-i18next';
+import i18n from 'i18next';
 
 jest.mock('../../config/env', () => ({
   API_BASE_URL: 'http://localhost:3000',
@@ -8,10 +10,25 @@ jest.mock('../../config/env', () => ({
   FAVORITES_STORE_NAM: 'favorites-store-name',
 }));
 
+beforeAll(() => {
+  i18n.init({
+    lng: 'en',
+    resources: {
+      en: {
+        translation: {},
+      },
+    },
+  });
+});
+
 describe('SearchInput Component', () => {
   test('renders input with placeholder', () => {
-    render(<SearchInput />);
-    const inputElement = screen.getByPlaceholderText('Search a character...');
+    render(
+      <I18nextProvider i18n={i18n}>
+        <SearchInput />
+      </I18nextProvider>
+    );
+    const inputElement = screen.getByRole('img');
     expect(inputElement).toBeInTheDocument();
   });
 });
